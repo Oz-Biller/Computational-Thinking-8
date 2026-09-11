c529ac69881e64ac24932b0b61296ed0389f8f9d
Oz-Biller <ozbiller@seattleacademy.org>
2026-09-11 18:50:08 +0000

    Testing my turn in system

---
+++ about_me.py
@@ -0,0 +1,9 @@
+print("Hello, I'm Oz!")
+name = input("What is your name?")
+print(f"Hello {name}, nice to meet you!")
+print("Let me share a bit about myself. I like music, games, sports, and being social with friends. What " 
+"about you? What do you like?")
+input()
+print("Nice!")
+print(" Well, I'm and 8th grader at Seattle Acedemy of Arts and Sciences, and this is my first year.")
+print("I've lived in Washington my whole life, and moved to West Seattle from Madrona when I was two.")
---
+++ jokes.py
@@ -0,0 +1,12 @@
+print("How do you know theres an elephant under your bed?")
+input()
+print("When your nose starts touching the ceiling")
+input()
+print("Wanna hear another?")
+answer = input()
+if answer == "no":
+    print("wow ur boring")
+elif answer == "yes":
+    print("what are hamsters called hamsters")
+input()
+print("cuz they look like guinnea PIGS! Get it, because it's a HAMster.")
